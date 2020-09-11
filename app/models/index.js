a2b188b5921617d40d25d858908805a972d63a32
Fairuza Raryasdya <fairuza.raryasdya@gmail.com>
2020-09-11 16:50:17 +0700

    Change database setting

--- app/models/index.js
+++ app/models/index.js
@@ -1,13 +1,7 @@
 require("dotenv").config();
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: true,
-  },
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const db = {};
 
