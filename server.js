a4648dd3a87beee9c611de708a221ec27dcbe238
Fairuza Raryasdya <fairuza.raryasdya@gmail.com>
2020-09-11 16:38:53 +0700

    Change SSO url

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/static"));
 
 const sso = new SSO({
-  url: process.env.URL || "http://localhost:3000", //required
+  url: process.env.URL || "http://localhost:3000", // required
   session_sso: "sso_user", // defaults to sso_user
 });
 
